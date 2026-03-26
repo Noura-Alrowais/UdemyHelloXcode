c8b6d74f7095bf2ce7d84e115749fe3f8567ab02
Noura <nalrowais98@gmail.com>
2026-03-26 10:31:25 +0300

    Section 1: Setup, UI Containers, add images, background, text allignment, and padding

--- UdemyHelloXcode/ContentView.swift
+++ UdemyHelloXcode/ContentView.swift
@@ -9,13 +9,83 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                colors: [
+                    Color.blue,
+                    Color.orange,
+                    Color.purple
+                ],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            ).ignoresSafeArea()
+            VStack {
+                Image(
+                    systemName: "iphone.gen2"
+                )
+                .imageScale(
+                    .large
+                )
+                Text(
+                    "Welcome to iOS Development!"
+                )
+                .font(
+                    .largeTitle
+                )
+                .fontWeight(
+                    .semibold
+                )
+                .multilineTextAlignment(
+                    .center
+                )
+                
+                Text(
+                    "First App"
+                )
+                .font(
+                    .title
+                )
+                .fontWeight(
+                    .thin
+                ).padding()
+                    .background(
+                        Color.yellow
+                            .opacity(
+                                0.5
+                            )
+                    )
+                Text(
+                    "I Love to Code!"
+                )
+                
+                
+                Text(
+                    "Please feel free to ask any question, send me a message via email"
+                )
+                .font(
+                    .title2
+                )
+                .fontWeight(
+                    .bold
+                )
+                .foregroundStyle(
+                    .indigo
+                )
+                .multilineTextAlignment(
+                    .center
+                )
+                .padding()
+                Image(
+                    "sunset"
+                )
+                .resizable()
+                .frame(
+                    width: 200.0,
+                    height: 200.0
+                )
+            }.padding()
+           
         }
-        .padding()
     }
 }
 
