cdf5a0a8ed04e2b71af0cf2feba88602492ff53d
Noura <nalrowais98@gmail.com>
2026-03-26 08:38:51 +0300

    Initial Commit

---
+++ UdemyHelloXcode/UdemyHelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemyHelloXcodeApp.swift
+//  UdemyHelloXcode
+//
+//  Created by Noura Alrowais on 07/10/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct UdemyHelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
